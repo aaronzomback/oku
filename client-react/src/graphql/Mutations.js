4a5079a7ad51b4ca671dc978593d97d009147804
aaronzomback <aaronzomback@gmail.com>
2021-07-31 23:09:09 +0200

    feat: custom fonts/rendering home screen

--- client-react/src/graphql/Mutations.js
+++ client-react/src/graphql/Mutations.js
@@ -1,3 +1,19 @@
 import { gql } from '@apollo/client';
 
-// place our app Mutations functions logic here
+export const CREATE_HAIKU_MUTATION = gql`
+  mutation createHaiku($content: String!) {
+    createHaiku(content: $content) {
+        id
+        content
+        author {
+          id
+          name
+          username
+        }
+        collected
+        submitted
+        featured
+        createdAt
+    }
+  }
+`
--- client-react/src/pages/Create.js
+++ client-react/src/pages/Create.js
@@ -1,6 +1,9 @@
 import HaikuForm from '../components/CreateHaikuForm';
+import { syllable } from 'syllable'
 
 function Create () {
+
+  console.log('syllables: ', syllable('hello there my name is dude.'));
 
   return (
     <section className='create-haiku'>
