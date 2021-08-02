bfe920f1a15ec4c1241e375ad478d2c5674a6d89
aaronzomback <aaronzomback@gmail.com>
2021-08-02 23:11:10 +0200

    feat: sync line1/2/3 schema to client

--- client-react-native/env.js
+++
@@ -1,5 +0,0 @@
-
-// Word of the Day API
-export const WORD_API_KEY = 'jmp126k8r122nkk0u357tyl2bp1j5yrh5l8a46sp086rj95a9';
-export const BASE_URL = 'https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key='
-
